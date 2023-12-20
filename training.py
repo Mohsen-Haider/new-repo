72d1083811bc47ea89b75c18c9334c9901cf517c
Mohsen <mohsen00091@gmail.com>
2023-12-20 12:19:36 +0300

    new commit at 12:19 PM

--- training.py
+++ training.py
@@ -5,6 +5,7 @@
 # I will change the time for the first comment 
 # New comment from githup
 # This is comment has been changed to time : 10:00 AM
+# This comment is created at 12:18 PM 
 
 x=[23,26,30,34,43,48,52,57,58] 
 y=[651,762,856,1063,1190,1298,1421,1440,1518]
